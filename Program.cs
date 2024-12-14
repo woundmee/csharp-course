cc5be959320d1eac00ec557d69d4f17a559f8869
woundmee <woundmee@gmail.com>
2024-12-14 17:32:16 +0700

    test

--- Program.cs
+++ Program.cs
@@ -1,29 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Runtime.InteropServices;
-using System.Runtime.Intrinsics.X86;
+
+
+
+/// Кажется, еще совсем недавно наступил новый 2013 год. А знали ли Вы забавный факт о том, что 2013 год является первым годом после далекого 1987 года, в котором все цифры различны?
+/// Теперь же Вам предлагается решить следующую задачу: задан номер года, найдите наименьший номер года, который строго больше заданного и в котором все цифры различны.
+
+/// Вход: В единственной строке задано целое число y (1000 ≤ y ≤ 9000)y (1000 ≤ y ≤ 9000) — номер года.
+/// Выход: Выведите единственное целое число — минимальный номер года, который строго больше y, в котором все цифры различны. Гарантируется, что ответ существует.
 
 public class MainClass
 {
     public static void Main()
     {
-        int a = int.Parse(Console.ReadLine()!);
-        int b = int.Parse(Console.ReadLine()!);
+        int year = int.Parse(Console.ReadLine()!);
 
-        for (int i = b; i >= a; i--)
+        var yearList = year.ToString().Select(x => int.Parse(x.ToString())).ToList();
+        for (int i = 0; i < yearList.Count(); i++)
         {
-            if (i % 7 == 0) 
+            if (yearList[i] == yearList[i+1])
             {
-                Console.WriteLine(i);
+                Console.WriteLine(yearList[i]);
                 break;
             }
-            
-            b--;
+            else Console.WriteLine("Eror: удалить условие");
         }
 
-        if (b == 0) Console.WriteLine("NO");
+        // for (int i = year; i <= 9000; i++)        
+        // {
+
+        // }
 
     }
 }
