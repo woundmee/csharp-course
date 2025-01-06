9be2818c93f923c1e35b2564f11b3fe7b64330f3
woundmee <woundmee@gmail.com>
2025-01-06 15:48:17 +0700

    двумерный массив: проверка на симметричность

--- Program.cs
+++ Program.cs
@@ -16,20 +16,64 @@ public class MainClass
 {
     static void Main()
     {
+        // ПРОВЕРКА НА СИММЕТРИЧНОСТЬ 2-МЕРНОГО МАССИВА.
+        // т.е. чтобы все значения (за исключением главной диагонали) были симметричны.
+
+        // для одномерного массива симметричность - это просто палиндром: 1 2 3 2 1
+
+
         // int n = int.Parse(Console.ReadLine()!);
         // string[] arr = Console.ReadLine()!.Split(' ');
 
-        int n = 5;
-        string[] arr = "1 2 1 3 1".Split(' ');
+        int n = 3;
+        int[,] arr = new int[n, n];
 
-        int[] arrInt = new int[arr.Length];
         for (int i = 0; i < n; i++)
-            arrInt[i] = int.Parse(arr[i]);
+        {
+            string[] input = Console.ReadLine()!.Split(' ');
+            for (int j = 0; j < n; j++)
+                arr[i, j] = int.Parse(input[j]);
+        }
 
-        var res = arrInt.GroupBy(x => x).Where(x => x.Count() == 1).Select(x => x.Key);
-        foreach(var item in res)
-            System.Console.Write(item + " ");
+        bool isSym = true;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (arr[i, j] != arr[j, i]) { isSym = false; break; }
+            }
+            if (!isSym) break;  // чтобы дальше цикл не гонять, т.к. итак уже фолс
+        }
+
+        Console.WriteLine(isSym ? "YES" : "NO");
 
     }
 
 }
+
+
+
+// int n = 10;
+
+//         int[,] arr = new int[n, n];
+
+//         for (int i = 0; i < n; i++)
+//         {
+//             for (int j = 0; j < n; j++)
+//             {
+//                 // NOTE: ОБРАТИТЬ ВНИМАНИЕ!
+//                 if (j == n - 1 - i) arr[i, j] = 1;
+//                 else if (j < n - i) arr[i, j] = 0;
+//                 else arr[i, j] = 2;
+//             }
+//         }
+
+
+//         for (int i = 0; i < n; i++)
+//         {
+//             for (int j = 0; j < n; j++)
+//             {
+//                 Console.Write(arr[i, j] + " ");
+//             }
+//             Console.WriteLine();
+//         }
