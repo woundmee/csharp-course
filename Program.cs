3c40b2a27d327f53383690718654cf21c1cd7709
woundmee <woundmee@gmail.com>
2025-01-10 17:33:02 +0700

    двумерный массив: задача Цветник

--- Program.cs
+++ Program.cs
@@ -17,55 +17,73 @@ public class MainClass
     static void Main()
     {
 
+        // Цветник
+        // Григорий занимается цветочным бизнесом. В его распоряжении имеется парник, поделённый на 15 участков (3*5), на каждом из которых растут розовые кусты. На одном участке могут расти цветы только одного вида. Григорий самостоятельно решает, какие цветы будут расти на каждом участке парника и по какой цене их отпускать.
+        // Известно, что фирма занимается выращиванием и реализацией только красных, белых и розовых роз и на каждом участке появляются ровно 100 цветов за год.
+        // Напишите программу, которая выведет ежегодную прибыль Григория. Всеми расходами пренебречь.
+        
+        // Входные данные
+            // В качестве входных данных вводится сначала таблица размером 3 на 5, заполненная буквами, обозначающими цвет посаженных на соответствующем участке роз. Так буква "к" обозначает красные розы, "б" - белые, "р" - розовые. 
+            // Далее вводятся 3 числа — цены за один цветок, по которым Григорий собирается продавать соответственно красные, белые и розовые розы.
+
+        // Выходные данные
+            // Программа должна вывести ежегодную прибыль Григория без учёта всех возможных расходов.
+
+        // Примечание:
+            // Распределение роз в парнике и их цены всегда остаются постоянными.
+            //  Каждая буква это 100 роз соответствующего цвета.
+
+        
+        // Sample Input 1:
+
+        // к к к к к
+        // б б б б б
+        // р р р р р
+        // 100 90 110
+        
+        // Sample Output 1:
+        // 150000
+
+
+
         // int n = int.Parse(Console.ReadLine()!);
         // string[] arr = Console.ReadLine()!.Split(' ');
 
         //string[] x = Console.ReadLine()!.Split(' ');
         // string[] arr = "1 2 3 -2 -4".Split(' ');
 
-        int n = 3; //int.Parse(x[0]);
-        int m = 3; //int.Parse(x[1]);
-        bool captcha = false;
 
-        int[,] arr = new int[n, m];
+        string[,] arr = new string[3, 5];
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < 3; i++)
         {
             string[] input = Console.ReadLine()!.Split(' ');
-            for (int j = 0; j < m; j++)
+            for (int j = 0; j < 5; j++)
             {
-                arr[i, j] = int.Parse(input[j]);
+                arr[i, j] = input[j];
             }
         }
 
-        int searchNumb = int.Parse(Console.ReadLine()!);  // необходимо найти в массиве
-        int attempt = int.Parse(Console.ReadLine()!);  // попытки
+        string[] prices = Console.ReadLine()!.Split(' ');
+        int priceRedRose = int.Parse(prices[0]);
+        int priceWhiteRose = int.Parse(prices[1]);
+        int pricePinkRose = int.Parse(prices[2]);
 
-        for (int i = 0; i < attempt; i++)
+        int sumRed = 0;
+        int sumWhite = 0;
+        int sumPink = 0;
+
+        for (int i = 0; i < 3; i++)
         {
-            string[] input = Console.ReadLine()!.Split(' ');
-            for (int j = 0; j < attempt; j++)
+            for (int j = 0; j < 5; j++)
             {
-                int ii = int.Parse(input[0]);
-                int jj = int.Parse(input[1]);
-                if (arr[ii, jj] == searchNumb)
-                {
-                    // Console.WriteLine(arr[ii, jj]);
-                    captcha = true;
-                }
-                else {
-                    captcha = false;
-                    break;
-                }
+                if (arr[i, j] == "к") sumRed += priceRedRose;
+                else if (arr[i, j] == "б") sumWhite += priceWhiteRose;
+                else sumPink += pricePinkRose;
             }
         }
 
-
-        Console.WriteLine(captcha ? "Captcha успешно пройдена" : "Вы не человек");
-
-
-
-
+        Console.WriteLine((sumRed + sumWhite + sumPink) * 100);
 
 
     }
